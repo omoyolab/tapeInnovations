46ac551f3835c13208c6c87e7cc9032dcbdd9c70
MySecondLanguage <mmamunism@gmail.com>
2020-08-07 23:34:49 +0600

    made popup on click

---
+++ app/assets/scripts/modules/popup.js
@@ -0,0 +1,20 @@
+var $ = require('jquery');
+
+
+$(document).ready(function() {
+    
+    $('.mtr-lm').click(function() {
+       
+        $('.pop-up').show(1000);
+        $('.html-container').prepend($(this).parent().parent().html())
+    });
+
+
+    $('.close-popup').click(function() {
+        $('.pop-up').hide(1000);
+        $('.html-container').empty();
+    })
+
+    
+
+})
