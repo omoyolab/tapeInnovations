f0c0f4d96007bc8e02abeb7afec69319a435df63
MySecondLanguage <mmamunism@gmail.com>
2020-08-08 11:10:38 +0600

    Fixed merge conflict and fixed popup hide

--- app/assets/scripts/modules/popup.js
+++ app/assets/scripts/modules/popup.js
@@ -4,15 +4,16 @@ var $ = require('jquery');
 $(document).ready(function() {
     
     $('.mtr-lm').click(function() {
-       
-        document.body.insertAdjacentHTML('beforeend', 
-            `
-                <div class="pop-up">
-                    ${$(this).parent().parent().html()}
-                </div>
-            `
-        )
+        $('.pop-up').show(1000);
+        $('.html-container').prepend($(this).parent().parent().html())
     });
+
+
+    $('.close-popup').click(function() {
+        $('.pop-up').hide(1000);
+        $('.html-container').empty();
+    })
+
 
     
 
