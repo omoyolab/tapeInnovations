51029b3291094d51486d6ca6d6083d96c95fc632
MySecondLanguage <mmamunism@gmail.com>
2020-08-10 15:48:41 +0600

    Refactored tabs to add new tab

--- app/assets/scripts/modules/utils.js
+++ app/assets/scripts/modules/utils.js
@@ -20,14 +20,14 @@ $(document).ready(function() {
     $('#firstbtn').addClass('active');
     $('#firstcontent').show();
     
-    $('.tablinks').click(function() {
+    $('.tablinkclick').click(function() {
 
         var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
+        tabcontent = document.getElementsByClassName("tabcontentclick");
         for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
         }
-        tablinks = document.getElementsByClassName("tablinks");
+        tablinks = document.getElementsByClassName("tablinkclick");
         for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
         }
@@ -36,5 +36,26 @@ $(document).ready(function() {
         // evt.currentTarget.className += " active";
         $(this).addClass("active")
 
-    })
+    });
+
+    $('#firstbtn2').addClass('active');
+    $('#firstcontent2').show();
+    
+    $('.tablinkclick2').click(function() {
+
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontentclick2");
+        for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinkclick2");
+        for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        // document.getElementById(cityName).style.display = "block";
+        $("#"+$(this).attr('data-target')).show();
+        // evt.currentTarget.className += " active";
+        $(this).addClass("active")
+
+    });
 })
